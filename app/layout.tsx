9b5fd222cc91e7f3db613b3352985cfaad40ed7b
Narei <ieranofficial@gmail.com>
2023-07-29 14:45:47 +0200

    small change

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en">
-			<body className={inter.className + ' flex flex-col-reverse md:flex-row items-center'}>
+			<body className={inter.className + ' flex flex-row'}>
 				<NavBar />
 				{children}
 			</body>

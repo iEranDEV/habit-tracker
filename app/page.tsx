5b327c99ea798b8f07492e47b86faf23f52a13fb
Narei <ieranofficial@gmail.com>
2023-07-30 21:30:21 +0200

    layout, planning

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,20 @@
 
 export default function Home() {
 	return (
-		<div className="w-[1000px]">
+		<div className="w-screen lg:w-[1024px] py-16">
 			<div className="bg-green-500 h-60">
 
 			</div>
 			<div className="w-full flex sticky top-0">
-				<div className="basis-2/3 bg-gradient-to-b from-blue-100 to-blue-900 h-[2000px]">
+				<div className="basis-full lg:basis-3/4 flex flex-col">
+					<div className="w-full h-40 bg-purple-500 sticky top-0">
+						
+					</div>
+					<div className="w-full bg-gradient-to-b from-blue-100 to-blue-900 h-[2000px]">
 
+					</div>
 				</div>
-				<div className="basis-1/3 sticky top-0 bg-yellow-500 h-[700px]">
+				<div className="hidden lg:block basis-1/4 sticky top-0 bg-yellow-500 h-[700px]">
 
 				</div>
 			</div>

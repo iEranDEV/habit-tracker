9197beb40442d5a34158f6c578261a3eef537be3
Narei <ieranofficial@gmail.com>
2023-08-07 15:32:13 +0200

    sign in & sign up, loading animation, auth form errors

--- firebase/auth.ts
+++ firebase/auth.ts
@@ -1,28 +1,23 @@
+import { FirebaseError } from "firebase/app";
 import firebase_app from "./config";
 import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from 'firebase/auth'
 
 const auth = getAuth(firebase_app);
 
 export async function signUp(email: string, password: string) {
-    let result = null,
-        error = null;
-    try {
-        result = await createUserWithEmailAndPassword(auth, email, password);
-    } catch (e) {
+    let result = null, error = null;
+    result = await createUserWithEmailAndPassword(auth, email, password).catch((e: FirebaseError) => {
         error = e;
-    }
+    });
 
     return { result, error };
 }
 
 export async function signIn(email: string, password: string) {
-    let result = null,
-        error = null;
-    try {
-        result = await signInWithEmailAndPassword(auth, email, password);
-    } catch (e) {
+    let result = null, error = null;
+    result = await signInWithEmailAndPassword(auth, email, password).catch((e: FirebaseError) => {
         error = e;
-    }
+    });
 
     return { result, error };
 }
